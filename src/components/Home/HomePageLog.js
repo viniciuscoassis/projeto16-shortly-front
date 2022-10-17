b655daf2a08efb74241ad014b071aac2a0d09aa0
vinicius <vinicius.coassis@gmail.com>
2022-10-17 11:47:01 -0300

    style: fix shortener input

--- src/components/Home/HomePageLog.js
+++ src/components/Home/HomePageLog.js
@@ -9,12 +9,54 @@ export default function HomePageLogged() {
       <Header />
       <ShortContainer>
         <form className="form">
-          <input className="form__input" />
-          <button className="form__button" />
+          <input
+            className="form__input u-green-box"
+            placeholder=" Links que cabem no bolso"
+          />
+          <button className="form__button u-default-button-green">
+            Encurtar Link
+          </button>
         </form>
+        <div className="container">
+          ola
+          <div className="container__entity">ola</div>
+        </div>
       </ShortContainer>
     </>
   );
 }
 
-const ShortContainer = styled.div``;
+const ShortContainer = styled.div`
+  height: 100%;
+  .form {
+    padding-top: 6rem;
+    display: flex;
+    width: 80vw;
+    margin: 0 auto;
+    justify-content: space-between;
+
+    &__input {
+      border: none;
+      display: flex;
+      margin: 0;
+      width: 60vw;
+
+      &::placeholder {
+        padding-left: 0.7rem;
+        color: #9c9c9c;
+      }
+    }
+    &__button {
+      margin: 0;
+    }
+  }
+
+  .container {
+    margin: 5rem auto;
+    width: 80vw;
+    min-height: 30vh;
+    display: flex;
+    justify-content: center;
+    background-color: orangered;
+  }
+`;
